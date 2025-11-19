856bd7dc9998501ff9a397917f820b8084165823
Moegammad Yaaseen Alexander <ST10453511@imconnect.edu.za>
2025-11-19 13:16:44 +0200

    add a user class in the models, this will be used to give roles to user identity's

---
+++ PROG6212_POE/Models/User.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PROG6212_POE.Models
+{
+    public class User
+    {
+        [Key]
+        public int UserId { get; set; }
+
+        [Required]
+        public string Username { get; set; } = string.Empty;
+
+        [Required]
+        public string Password { get; set; } = string.Empty; // Storing plain for academic demo, usually hashed
+
+        [Required]
+        public string Role { get; set; } = string.Empty; // Lecturer, ProgrammeCoordinator, AcademicManager, HR
+
+        public string FullName { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+
+        // Automating the rate: HR sets this, Lecturer just uses it.
+        public decimal HourlyRate { get; set; } = 0m;
+    }
+}
--- PROG6212_POE/obj/Debug/net9.0/PROG6212_POE.AssemblyInfo.cs
+++ PROG6212_POE/obj/Debug/net9.0/PROG6212_POE.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PROG6212_POE")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+76f2a75b76a1862d0619c80d1936ba60ed9a5807")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fc06bfaf3067bed310a9acff32587cd2d4315eaf")]
 [assembly: System.Reflection.AssemblyProductAttribute("PROG6212_POE")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PROG6212_POE")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
